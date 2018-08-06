3a40324e39c16696b936bda14ad9d17e1bf778ab
BGluth <gluthb@gmail.com>
2018-08-05 22:44:00 -0600

    Added titles for both the input and output windows

--- curses_state.py
+++ curses_state.py
@@ -5,22 +5,49 @@ from curses.textpad import Textbox
 import drawing
 
 _INPUT_TEXT_NUM_LINES = 1
+_TITLE_HEIGHT = 1
+_DASH_TITLE_LENGTH_PERC = 0.50
 
 
 class CursesState:
     def __init__(self, main_window):
         self.main_win = main_window
 
-        screen_y, screen_x = self.main_win.getmaxyx()
-        output_win_num_lines = screen_y - _INPUT_TEXT_NUM_LINES
-        input_win_begin_y = output_win_num_lines
+        screen_max_y, screen_max_x = self.main_win.getmaxyx()
+        output_win_num_lines = screen_max_y - _INPUT_TEXT_NUM_LINES - _TITLE_HEIGHT * 2
 
-        self.output_win = curses.newwin(output_win_num_lines, screen_x, 0, 0)
-        self.input_win = curses.newwin(_INPUT_TEXT_NUM_LINES, screen_x, input_win_begin_y, 0)
+        output_title_beg_y = 0
+        output_win_beg_y = output_title_beg_y + _TITLE_HEIGHT
+        input_title_beg_y = output_win_beg_y + output_win_num_lines
+        input_win_beg_y = input_title_beg_y + _TITLE_HEIGHT
+
+        self.output_win_title = curses.newwin(_TITLE_HEIGHT, screen_max_x, output_title_beg_y, 0)
+        self.output_win = curses.newwin(output_win_num_lines, screen_max_x, output_win_beg_y, 0)
+        self.input_win_title = curses.newwin(_TITLE_HEIGHT, screen_max_x, input_title_beg_y, 0)
+        self.input_win = curses.newwin(_INPUT_TEXT_NUM_LINES, screen_max_x, input_win_beg_y, 0)
+
         self.input_win_tb = Textbox(self.input_win)
         self.input_win_tb.stripspaces = True  # Seems to be broken...
 
+        self._init_title_texts()
+
         self.output_win.scrollok(True)
+
+    def _init_title_texts(self):
+        self._init_title_text(self.output_win_title, " OUTPUT ")
+        self._init_title_text(self.input_win_title, " INPUT ")
+
+    @staticmethod
+    def _init_title_text(win, text):
+        win_y, win_x = win.getmaxyx()
+        half_text_len = (len(text) // 2)
+        max_dashes_per_side = int(_DASH_TITLE_LENGTH_PERC * win_x / 2)
+        num_dashes_per_side = max_dashes_per_side - half_text_len
+        title_text = ("-" * num_dashes_per_side) + text + ("-" * num_dashes_per_side)
+        start_x = (win_x // 2) - half_text_len - num_dashes_per_side
+
+        win.addstr(0, start_x, title_text)
+        win.refresh()
 
 
 _state = None
