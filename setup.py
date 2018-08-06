399da77308c668ca27ce0f3e820eea022ee25198
BGluth <gluthb@gmail.com>
2018-08-06 14:50:57 -0600

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cubesatcomm",
+    version="0.0.1",
+    author="Brendan Gluth",
+    author_email="gluthb@gmail.com",
+    description="Simple terminal program to communicate with cube satelites",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
