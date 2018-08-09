4551b641f911188561a695512ba07510479eb16b
BGluth <gluthb@gmail.com>
2018-08-08 20:15:54 -0600

    Now catching all exceptions (and displaying them nicely) in main
    - Doing this in order to stop threads properly and make sure curses is able to put the terminal back to a sane state before exiting.

--- cube_sat_comm/main.py
+++ cube_sat_comm/main.py
@@ -1,3 +1,4 @@
+import traceback
 import curses
 
 from cube_sat_comm import handle_user_input
@@ -5,15 +6,27 @@ from cube_sat_comm import drawing
 from cube_sat_comm import curses_state
 from cube_sat_comm import commands
 
-def main(main_window):
-    curses_state.init_state(main_window)
-    commands.init_commands()
-    drawing_handle = drawing.init_drawing_thread()
 
-    handle_user_input.handle_input_loop()  # Main thread handles user input
+def main():
+    drawing_handle = None
+    try:
+        drawing_handle = drawing.init_drawing_thread()
+        commands.init_commands()
+
+        curses.wrapper(_run)
+
+    except Exception as ex:
+        print("The following exception has occurred: {}".format(ex))
+        print("Stacktrace: {}".format(traceback.format_exc()))
 
     drawing.stop_drawing_thread()
     drawing_handle.join()
 
 
-curses.wrapper(main)
+def _run(main_window):
+    curses_state.init_state(main_window)
+    handle_user_input.handle_input_loop()  # Main thread handles user input
+
+
+main()
+
